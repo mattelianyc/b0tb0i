572588832f839110d42478d18524c7a6d223b068
Matt Elia <nycsoftwaredude@gmail.com>
2022-04-24 21:18:26 -0600

    init

---
+++ app/main/puppet.js
@@ -0,0 +1,17 @@
+const { ipcMain } = require( 'electron' );
+const path = require( 'path' );
+const fs = require( 'fs-extra' );
+const os = require( 'os' );
+const { exec } = require('child_process');
+
+exports.runBot = () => {
+  console.log('bottyboi')
+  exec('node ./app/main/bots/amazon.js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
